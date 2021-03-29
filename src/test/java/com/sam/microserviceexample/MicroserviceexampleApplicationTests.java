2073e6c7d21ebc44c294edd023c5b29d533d28ea
sameesh-s <sameesh.fisat@gmail.com>
2021-03-29 16:03:48 +0530

    First java files

---
+++ src/test/java/com/sam/microserviceexample/MicroserviceexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.microserviceexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
